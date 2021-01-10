ce5f1f0bb3ec27e8286882859812d8e948e9792d
Taylor Callsen <gritto2008@gmail.com>
2021-01-09 22:08:42 -0800

    adding logic to get s3 presigned url

--- routes/image.js
+++ routes/image.js
@@ -3,10 +3,38 @@ const router = express.Router()
 
 const verifyToken = require('./token').verifyToken;
 
+const AWS = require('aws-sdk')
+AWS.config.update({
+  apiVersion: "2010-12-01",
+  accessKeyId: process.env.s3AccessKey,
+  secretAccessKey: process.env.s3SecretKey,
+  region: process.env.s3region
+})
+AWS.config.region = 'us-west-1';
+const s3 = new AWS.S3()
+const myBucket = process.env.s3Bucket
+
 // get protected image from s3 via pre-signed URL
-router.get('/', function(req, res, next) {
+router.get('/*', function(req, res, next) {
   if (verifyToken(req)) {
-    res.json({ success: true, status: 'image token verified' })
+
+    const myKey = req.url.substring(1) // get s3 key from request path - remove first slash
+    const signedUrlExpireSeconds = 60 * 10
+
+    s3.getSignedUrl('getObject', {
+      Bucket: myBucket,
+      Key: myKey,
+      Expires: signedUrlExpireSeconds
+    }, function (err, url) {
+      if (!err) res.redirect(url)
+      else {
+        res.status(500).json({
+          success: false,
+          status: err,
+          url: url
+        })
+      }
+    })
   } else res.status(401).json({ success: false, status: 'failed to verify token' })
 })
 
