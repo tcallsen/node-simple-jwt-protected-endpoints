f13cecaf4143bd16818453193e4478b42a0c6913
Taylor Callsen <gritto2008@gmail.com>
2021-01-09 20:26:02 -0800

    adding jwt token creation and verification

--- routes/token.js
+++ routes/token.js
@@ -1,9 +1,34 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-/* GET token. */
+var jwt = require('jsonwebtoken')
+var fs = require('fs')
+
+var secretKey = fs.readFileSync(__dirname + '/../privateKey.pem', 'utf8');
+var publicCert = fs.readFileSync(__dirname + '/../publicKey.pem', 'utf8');
+
+// get token from certificate private key
 router.get('/', function(req, res, next) {
-  res.send('getToken');
-});
+  var payload = { foo: 'bar' };
+  var token = jwt.sign(payload, secretKey, { algorithm: 'RS256' });
+  
+  res.json({ success: true, access_token: token })
+})
 
-module.exports = router;
+// verify token against certificate public key
+router.get('/verify', function(req, res, next) {
+  try {
+    const authorizationHeader = req.headers['authorization']
+    const authorziation = authorizationHeader.split(' ');
+    const bearerToken = authorziation[1];
+    jwt.verify(bearerToken, publicCert, { algorithm: 'RS256' })
+
+    res.json({ success: true, status: 'token verified' })
+  } catch (e) {
+    console.error('failed to parse token - may be missing Authorization header', e)
+  }
+  
+  res.status(401).json({ success: false, status: 'failed to verify token' });
+})
+
+module.exports = router
