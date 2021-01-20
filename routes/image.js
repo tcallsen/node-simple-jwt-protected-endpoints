0350a5b3bca9987f1653b03fcbad015e0a2b332b
Taylor Callsen <gritto2008@gmail.com>
2021-01-20 06:03:00 -0800

    adding route to retrieve image metdata

--- routes/image.js
+++ routes/image.js
@@ -1,19 +1,26 @@
 const express = require('express')
 const router = express.Router()
 
-const verifyToken = require('../middlewares/verifyToken')
-
 const fs = require('fs')
+const path = require('path')
+
+const verifyToken = require('../middlewares/verifyToken')
 
 // get image
 router.get('/', verifyToken, function(req, res, next) {
 
-  const s = fs.createReadStream('./sample_data/image.jpg');
+  const s = fs.createReadStream(path.join(__dirname, '..', 'sample_data', 'image.jpg'))
   s.on('open', function () {
     res.set('Content-Type', 'image/jpeg');
     s.pipe(res)
   })
   
+})
+
+// get image metadata
+router.get('/metadata', verifyToken, function(req, res, next) {
+  res.setHeader('Content-Type', 'application/json')
+  res.sendFile(path.join(__dirname, '..', 'sample_data' , 'metadata.json'))
 })
 
 module.exports = router
